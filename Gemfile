fc22abb0bc7dff15fba10d60b17cc00703c1d296
Ash Furrow <ash@ashfurrow.com>
2016-07-01 16:07:13 -0400

    Parameterizes everything.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'json'
+gem 'octokit'
--- server.rb
+++ server.rb
@@ -1,21 +1,38 @@
 require 'sinatra'
 require 'json'
+require 'octokit'
 
 class AerynApp < Sinatra::Base
-  get '/' do
-    "Hello, world!"
-  end
-
-  post '/payload' do
+  post ENV['WEBHOOK_ENDPOINT'] do
     request.body.rewind
     payload_body = request.body.read
     verify_signature(payload_body)
     push = JSON.parse(payload_body)
-    puts "I got some JSON: #{push.inspect}"
+
+    return unless is_merged(push)
+    username = push['pull_request']['user']['login']
+
+    client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+    if client.organization_member?(ENV['ORG_NAME'], username)
+      puts "Already a member."
+    else
+      puts "Inviting #{username} to team."
+
+      add_team_membership(ENV['CONTRIBUTOR_TEAM_ID'], username)
+
+      pr_number = push['pull_request']['number']
+      repo_name = push['pull_request']['base']['repo']['full_name']
+      add_comment(repo_name, pr_number, ENV['INVITATION_MESSAGE'])
+    end
+
+  end
+
+  def is_merged(push)
+    return push['action'] == 'closed' && push['pull_request']['merged'] == true
   end
 
   def verify_signature(payload_body)
-    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_TOKEN'], payload_body)
     return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 
