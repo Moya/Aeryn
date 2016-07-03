f7a07dab4e34832c0a4cd09f75f9b412cd9a22e3
Ash Furrow <ash@ashfurrow.com>
2016-07-03 10:15:52 -0400

    Doesn't re-issue invitation & comment for users who are already invited.

--- lib/api.rb
+++ lib/api.rb
@@ -13,26 +13,39 @@ class API
     if is_merged?(push)
       pull_request = push['pull_request']
       username = pull_request['user']['login']
+      team_id = ENV['CONTRIBUTOR_TEAM_ID']
+      pr_number = pull_request['number']
+      repo_name = pull_request['base']['repo']['full_name']
       
-      # Should check invitation status too/instead
-      if @github_client.organization_member?(ENV['ORG_NAME'], username)
-        return {'msg' => 'Already a member.'}
-      else
-        team_id = ENV['CONTRIBUTOR_TEAM_ID']
-
-        @github_client.add_team_membership(team_id, username)
-
-        pr_number = pull_request['number']
-        repo_name = pull_request['base']['repo']['full_name']
-        @github_client.add_comment(repo_name, pr_number, ENV['INVITATION_MESSAGE'])
+      if needs_invitation?(team_id, username)
+        invite_and_comment(team_id, username, pr_number, repo_name)
         return {'msg' => 'Invitation sent.'}
+      else
+        return {'msg' => 'Already a member.'}
       end
     else
       return {'msg' => 'Pull request not yet merged.'}
     end
   end
 
+
   def is_merged?(push)
     push['action'] == 'closed' && push['pull_request'] && push['pull_request']['merged'] == true
+  end
+
+  def needs_invitation?(team_id, username)
+    begin
+      # This raises if the user has not been invited yet, as the happy
+      # path is 'user invitation is pending' or 'user has accepted'.
+      @github_client.team_membership(team_id, username)
+      return false
+    rescue Octokit::NotFound
+      return true
+    end
+  end
+
+  def invite_and_comment(team_id, username, pr_number, repo_name)
+    @github_client.add_team_membership(team_id, username)
+    @github_client.add_comment(repo_name, pr_number, ENV['INVITATION_MESSAGE'])
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -14,20 +14,21 @@ describe 'API' do
   end
 
   it 'does not re-invite members' do
-    allow(github_client).to receive(:organization_member?).with('Organization', 'splendid_username').and_return(true)
+    allow(github_client).to receive(:team_membership).with('1234567', 'splendid_username')
     
     result = api.handle_push({'action' => 'closed', 
       'pull_request' => {
         'merged' => true, 
         'number' => 13,
-        'user' => {'login' => 'splendid_username'}
+        'user' => {'login' => 'splendid_username'},
+        'base' => {'repo' => {'full_name' => 'my_repo'}}
         }})
 
     expect(result['msg']).to eq('Already a member.')
   end
 
   it 'adds team membership and sends a friendly comment' do
-    allow(github_client).to receive(:organization_member?).with('Organization', 'splendid_username').and_return(false)
+    allow(github_client).to receive(:team_membership).with('1234567', 'splendid_username').and_raise (Octokit::NotFound)
     allow(github_client).to receive(:add_team_membership).with('1234567', 'splendid_username')
     allow(github_client).to receive(:add_comment).with('my_repo', 13, 'Thanks!')
 
