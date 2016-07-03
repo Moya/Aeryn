3802c57f2b4e62af291fa0b3102c26915375265f
Ash Furrow <ash@ashfurrow.com>
2016-07-02 21:08:04 -0400

    Removed old comment.

--- lib/signature_verifier.rb
+++ lib/signature_verifier.rb
@@ -3,7 +3,6 @@ require 'sinatra'
 class SignatureVerifier
 
   def verify_signature(payload_body, request)
-    # TODO: Handle this better, add logging, see if we can halt Sinatra request from within this method.
     signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_TOKEN'], payload_body)
     header = request.env['HTTP_X_HUB_SIGNATURE']
     return false unless header
